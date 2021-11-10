4625339b3da57be0991c08616452454a87db1ebc
ricardo-limaco <ricardolimaco.05@gmail.com>
2021-11-10 23:52:14 +0100

    Jumbotron

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <Title/>
+    <Hero/>
   </div>
 </template>
 
 
 <script>
 import Title from "./components/Title.vue";
+import Hero from "./components/Hero.vue";
 
 export default {
   name: 'App',
   components: {
-    Title
+    Title,
+    Hero,
   }
 }
 </script>
---
+++ src/components/Hero.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="m-auto mt-5 w-25">
+  <div class="bg-dark rounded-3 text-white text-start p-5 h-100">
+    <h2>Jumbotron</h2>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+      Atque earum nisi possimus hic...</p>
+    <button class="btn btn-outline-light">Button</button>  
+  </div>
+</div>
+</template>
+
+
+<script>
+export default {
+  name: 'Hero',
+};
+</script>
+
+<style>
+</style>
